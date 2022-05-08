33454028a1782614eff235a1efb8b83cd4be8e8e
Song, Yun-Zhu <45812808+yunzhusong@users.noreply.github.com>
2022-05-09 00:45:32 +0800

    Update run.sh

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=3
+export CUDA_VISIBLE_DEVICES=0
 
 # 1. Finetune abstractor with oracle input
 #python main.py ./scripts/args/finetune_abs.json
@@ -10,7 +10,7 @@ export CUDA_VISIBLE_DEVICES=3
 #python main.py ./scripts/args/train_ext_rl.json
 
 # 4. Obtain the predicted extraction results
-python main.py ./scripts/args/pred.json
+#python main.py ./scripts/args/pred.json
 
 # 5. Evaluate the extraction results
 python main.py ./scripts/args/eval.json
